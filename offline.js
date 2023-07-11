73895cd5d733e036dd3cfb380532ee387dcef793
Mateus Alves <98139059+Redyf@users.noreply.github.com>
2023-07-11 11:49:29 -0300

    Upload do jogo

---
+++ offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1689086761,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/geladeira1.png",
+		"images/geladeira2.png",
+		"images/geladeira3.png",
+		"images/prateleira-sheet0.png",
+		"images/faca-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"images/camera.png",
+		"images/brócolis-sheet0.png",
+		"images/pão-sheet0.png",
+		"images/carne-sheet0.png",
+		"images/faquinha-sheet0.png",
+		"images/milha-sheet0.png",
+		"images/ervilho-sheet0.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/tiledbackground3.png",
+		"images/sprite-sheet0.png",
+		"images/tiledbackground5.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
